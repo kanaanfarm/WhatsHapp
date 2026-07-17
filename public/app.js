fd63a2ce02afabb08e5a23005c50f61075eeefc2
kanaanfarm <kanaan3@hotmail.com>
2026-07-17 13:34:34 +0400

    Delete public/app.js

--- public/app.js
+++
@@ -1,8 +0,0 @@
-const CACHE="connectchat-pro-v2";
-const ASSETS=["/","/index.html","/style.css","/app.js","/manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
