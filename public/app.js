1ca0005f37a01f738238fe9d938dbad465c3e5d3
kanaanfarm <kanaan3@hotmail.com>
2026-07-27 10:33:25 +0400

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -156,7 +156,7 @@ async function showMessageNotification(title,body,tag){
     badge:"/logo.svg",
     tag,
     renotify:true,
-    data:{url:"/?v=6769"}
+    data:{url:"/?v=6770"}
   };
   try{
     if("serviceWorker" in navigator){
